782504b87d68b5fb03a64a23c12e28f6eb694122
antoine.thach <antoine.thach@versusmind.eu>
2017-04-25 16:09:44 +0200

    Adding Edit and Index method in MoviesController

--- AspTest/Controllers/MoviesController.cs
+++ AspTest/Controllers/MoviesController.cs
@@ -5,6 +5,21 @@ using System.Web;
 using System.Web.Mvc;
 using AspTest.Models;
 
+/*
+ * Action Results - HelperMethod
+ * ViewResult - View()
+ * PartialViewResult - PartialView()
+ * ContentResult - Content()
+ * RedirectResult - Redirect()
+ * RedirectToRouteResult - RedirectToAction()
+ * JsonResult - Json()
+ * FileResult - File()
+ * HttpNotFoundResult - HttpNotFound()
+ * EmptyResult   
+ * 
+ */
+
+
 namespace AspTest.Controllers
 {
     public class MoviesController : Controller
@@ -13,7 +28,30 @@ namespace AspTest.Controllers
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek!" };
-            return View(movie);
+            //return View(movie);
+            //return Content("Hello World !");
+            //return HttpNotFound();
+            //return new EmptyResult();
+            return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name"});
+        }
+
+        public ActionResult Edit(int Id)
+        {
+            return Content("Id = " + Id);
+        }
+
+        //movies
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+            if (String.IsNullOrWhiteSpace(sortBy))
+            {
+                sortBy = "Name";
+            }
+            return Content(String.Format("pageIndex={0}&sortBy{1}", pageIndex, sortBy));
         }
     }
 }
